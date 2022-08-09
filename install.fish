c2ccb859b6b9a511567ee89e67bf174a5d450b95
Vincent Quenneville-Belair <vincentqb@gmail.com>
2022-08-09 12:16:28 -0400

    cosmetic: move node.js lower.

--- install.fish
+++ install.fish
@@ -6,13 +6,6 @@ fish_add_path $HOME/bin $HOME/.local/bin $HOME/homebrew/bin $HOME/homebrew/sbin 
 # Install plugin manager
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 
-# Configure nvm
-fisher install jorgebucaran/nvm.fish
-nvm install latest
-npm install -g neovim yarn
-npm install -g bash-language-server diagnostic-languageserver
-yarn global add neovim
-
 # Configure prompt
 # fish_config prompt choose terlar
 # fisher install dracula/fish
@@ -32,6 +25,13 @@ set -U GIT_EDITOR nvim
 # Point vim to nvim in userspace
 alias vim="nvim"
 funcsave vim
+
+# Configure node.js to use with language servers
+fisher install jorgebucaran/nvm.fish
+nvm install latest
+npm install -g neovim yarn
+npm install -g bash-language-server diagnostic-languageserver
+yarn global add neovim
 
 # Install anaconda for fish
 conda init fish
