a6f2b27eb9dcc435dfc0bd2c97474c161f8451ee
Vincent Quenneville-Belair <vincentqb@gmail.com>
2022-09-12 18:02:18 -0400

    add toolbox to path.

--- install.fish
+++ install.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/fish
 
 # Extend $PATH
-fish_add_path $HOME/bin $HOME/.local/bin $HOME/homebrew/bin $HOME/homebrew/sbin /opt/homebrew/bin
+fish_add_path $HOME/bin $HOME/.local/bin $HOME/homebrew/bin $HOME/homebrew/sbin /opt/homebrew/bin $HOME/.toolbox/bin
 
 # Install plugin manager
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
