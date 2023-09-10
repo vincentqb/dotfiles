2ee42e9f864554c3cae0e51b0bc4d39cc35790e2
Vincent Quenneville-Belair <vincentqb@gmail.com>
2023-09-10 16:24:18 -0400

    add headless to nvim extension install cmd

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -10,10 +10,10 @@ git submodule update --init --recursive
 mkdir -p ~/.local/bin
 wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage -O ~/.local/bin/nvim
 chmod +x ~/.local/bin/nvim
-
-# Update vim plugins
-~/.local/bin/nvim +PackClean +qa
-~/.local/bin/nvim +PackUpdate +qa
+# Update nvim plugins
+~/.local/bin/nvim --headless +DirtytalkUpdate +qa
+~/.local/bin/nvim --headless +PackClean +qa
+~/.local/bin/nvim --headless +PackUpdate +qa
 
 # https://launchpad.net/~fish-shell/+archive/ubuntu/release-3
 sudo apt-add-repository ppa:fish-shell/release-3
