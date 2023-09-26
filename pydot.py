8e95e71b5e3e634c6ea0d69d3f0abac4e73fce8e
Vincent Quenneville-Belair <vincentqb@gmail.com>
2023-09-26 09:15:30 -0400

    merging map function

--- pydot.py
+++ pydot.py
@@ -52,72 +52,44 @@ def make_link(candidate, dotfile, rendered, dry_run):
     return True
 
 
-def link(candidates, dry_run):
+def link(candidate, dotfile, rendered, dry_run):
     """
     Link dotfiles to files in given folders in an idempotent way.
     """
 
-    success = True
-
-    for candidate, (dotfile, rendered) in candidates.items():
-        success = all(
-            [
-                success,
-                make_render(candidate, dotfile, rendered, dry_run),
-                make_link(candidate, dotfile, rendered, dry_run),
-            ]
-        )
-
-    return success
+    return all(
+        [
+            make_render(candidate, dotfile, rendered, dry_run),
+            make_link(candidate, dotfile, rendered, dry_run),
+        ]
+    )
 
 
-def unlink(candidates, dry_run):
+def unlink(candidate, dotfile, rendered, dry_run):
     """
     Unlink dotfiles that are linked to files in given folders.
     """
-    success = True
 
-    for candidate, (dotfile, rendered) in candidates.items():
-        if dotfile.exists():
-            if dotfile.is_symlink():
-                link = os.readlink(str(dotfile))
-                if link == str(rendered):
-                    if dry_run:
-                        logger.info(f"File {dotfile} would be unlinked from {rendered}")
-                    else:
-                        dotfile.unlink()
-                        logger.info(f"File {dotfile} unlinked from {rendered}")
+    if dotfile.exists():
+        if dotfile.is_symlink():
+            link = os.readlink(str(dotfile))
+            if link == str(rendered):
+                if dry_run:
+                    logger.info(f"File {dotfile} would be unlinked from {rendered}")
                 else:
-                    logger.warning(f"File {dotfile} exists and points to {link} instead of {rendered}")
-                    success = False
+                    dotfile.unlink()
+                    logger.info(f"File {dotfile} unlinked from {rendered}")
             else:
-                logger.warning(f"File {dotfile} exists but is not a link")
-                success = False
+                logger.warning(f"File {dotfile} exists and points to {link} instead of {rendered}")
+                return False
         else:
-            logger.warning(f"File {dotfile} does not exists")
-            success = False
+            logger.warning(f"File {dotfile} exists but is not a link")
+            return False
+    else:
+        logger.warning(f"File {dotfile} does not exists")
+        return False
 
-    return success
-
-
-def build_cdr_map(home, candidates):
-    """
-    Maps "candidate to be linked to" to ("dotfile", "rendered candidate")
-    """
-    TEMPLATE = ".template"
-    RENDERED = ".rendered"
-
-    names = {}
-
-    for candidate in candidates:
-        name = candidate.name
-        if not (name.startswith(".") or name.endswith(RENDERED)):
-            # Add dot prefix and replace template when needed
-            rendered = candidate.parent / re.sub(TEMPLATE + "$", RENDERED, name)
-            dotfile = home / ("." + re.sub(TEMPLATE + "$", "", name))
-            names[candidate] = (dotfile, rendered)
-
-    return names
+    return True
 
 
 def apply_command_to_folders(command, folders, dry_run):
@@ -128,9 +100,13 @@ def apply_command_to_folders(command, folders, dry_run):
         folder = Path(folder)
         folder = folder.expanduser().resolve()
         if folder.exists and folder.is_dir():
-            candidates = sorted(folder.glob("*"))
-            candidates = build_cdr_map(home, candidates)
-            success = all([success, command(candidates, dry_run)])
+            for candidate in sorted(folder.glob("*")):
+                name = candidate.name
+                if not (name.startswith(".") or name.endswith(".rendered")):
+                    # Add dot prefix and replace template when needed
+                    rendered = candidate.parent / re.sub(".template$", ".rendered", name)
+                    dotfile = home / ("." + re.sub(".template$", "", name))
+                    success = all([success, command(candidate, dotfile, rendered, dry_run)])
         else:
             logger.warning(f"Folder {folder} does not exist")
             success = False
