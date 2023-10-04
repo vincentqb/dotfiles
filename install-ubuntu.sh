417abb588b0cc40f004d1187ab3937bd7f652675
Vincent Quenneville-Belair <vincentqb@gmail.com>
2023-10-04 00:39:04 -0400

    get tmux appimage

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -7,6 +7,7 @@ git submodule update --init --recursive
 /usr/bin/pip3 install --user -r requirements.txt
 
 # Install latest neovim
+sudo apt -y install libfuse2
 mkdir -p ~/.local/bin
 wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage -O ~/.local/bin/nvim
 chmod +x ~/.local/bin/nvim
@@ -15,14 +16,22 @@ chmod +x ~/.local/bin/nvim
 ~/.local/bin/nvim --headless +PackUpdate +qa
 ~/.local/bin/nvim --headless +DirtytalkUpdate +qa
 
+# https://github.com/kiyoon/tmux-appimage
+curl -s https://api.github.com/repos/kiyoon/tmux-appimage/releases/latest \
+| grep "browser_download_url.*appimage" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| wget -qi - \
+&& chmod +x tmux.appimage && mv ./tmux.appimage ~/.local/bin/tmux
+
 # https://launchpad.net/~fish-shell/+archive/ubuntu/release-3
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt update
-sudo apt install fish
+sudo apt -y install fish
 
-sudo apt install tmux shellcheck texlive node
+sudo apt -y install tmux shellcheck texlive node
 
-sudo apt install fdfind bat ripgrep
+sudo apt -y install fdfind bat ripgrep
 
 ~/.tmux/plugins/tpm/bin/update_plugins all	# prefix + U
 ~/.tmux/plugins/tpm/bin/clean_plugins	    # prefix + alt + u
