d1127ac13e17684264a34bdb96996245e80a2b8b
Vincent Quenneville-Belair <vincentqb@gmail.com>
2023-10-10 23:07:39 -0400

    moving bash and ssh out of default

--- default/config/fish/config.fish
+++ default/config/fish/config.fish
@@ -18,3 +18,11 @@ if status is-interactive
     end
 
 end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/vincent/miniconda/bin/conda
+    eval /home/vincent/miniconda/bin/conda "shell.fish" "hook" $argv | source
+end
+# <<< conda initialize <<<
+
