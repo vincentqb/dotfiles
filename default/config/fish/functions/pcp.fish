13aa507ac3ba2ffb884e9530d44c95e1f8942996
Vincent QB <vincentqb@gmail.com>
2024-04-17 16:59:19 -0400

    more human readable cp

--- default/config/fish/functions/pcp.fish
+++ default/config/fish/functions/pcp.fish
@@ -1,4 +1,4 @@
-function pcp --wraps='rsync -a --info=progress2' --description 'alias pcp=rsync -a --info=progress2'
-  rsync -a --info=progress2 $argv
+function pcp --wraps='rsync -ahrz --info=progress2' --description 'alias pcp=rsync -ahrz --info=progress2'
+  rsync -ahrz --info=progress2 $argv
         
 end
