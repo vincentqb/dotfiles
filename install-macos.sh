b6d382ae29f4567fedbf46c4872c1f186f34c5af
Vincent QB <vincentqb@gmail.com>
2024-06-27 16:24:14 -0400

    rsync may need an update

--- install-macos.sh
+++ install-macos.sh
@@ -25,6 +25,7 @@ brew install --cask mactex-no-gui
 brew install yarn
 brew install wget
 brew install ruff
+brew install rsync
 
 # sshfs https://github.com/telepresenceio/telepresence/issues/1654#issuecomment-873538291
 # brew install --cask macfuse
