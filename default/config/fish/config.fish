a8b4aece747ef0bd99f73a33b1d6a9084fee047f
Vincent Quenneville-Belair <vincentqb@gmail.com>
2024-12-04 15:41:16 -0500

    remove conda

--- default/config/fish/config.fish
+++ default/config/fish/config.fish
@@ -18,11 +18,3 @@ if status is-interactive
     end
 
 end
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /home/vincent/miniconda/bin/conda
-    eval /home/vincent/miniconda/bin/conda "shell.fish" "hook" $argv | source
-end
-# <<< conda initialize <<<
-
