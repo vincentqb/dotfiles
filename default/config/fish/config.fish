c0920e539d8309497977dedb59b49e070b705638
Vincent Quenneville-Belair <vincentqb@gmail.com>
2024-12-06 15:07:33 -0800

    disable old alias

--- default/config/fish/config.fish
+++ default/config/fish/config.fish
@@ -3,11 +3,11 @@ if status is-interactive
     # command -v nvm || nvm install lts > /dev/null
 
     # Point vim to nvim in userspace
-    abbr v "nvim -O"
-    abbr vi "nvim -O"
+    # abbr v "nvim -O"
+    # abbr vi "nvim -O"
     abbr vim "nvim -O"
     abbr vimdiff "nvim -d"
-    abbr view "nvim -RO"
+    # abbr view "nvim -RO"
     abbr s "kitty +kitten ssh"
     abbr icat "kitty +kitten icat"
 
