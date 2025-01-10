76d313af458fcb62d40be7c3d06d12e9d3be0bc6
Vincent Quenneville-Belair <vincentqb@gmail.com>
2025-01-10 17:41:37 -0500

    wezterm

---
+++ default/config/wezterm/wezterm.lua
@@ -0,0 +1,20 @@
+local wezterm = require 'wezterm'
+
+return {
+    -- default_prog = { '/usr/local/bin/fish', '-l' }
+    default_prog = { '/opt/homebrew/bin/fish', '-l' },
+    font = wezterm.font 'Menlo',
+    -- font = wezterm.font 'Fira Code',
+    -- font = wezterm.font 'JetBrains Mono',
+    font_size = 14.0,
+    color_scheme = "Dracula (Official)",
+    tab_bar_at_bottom = true,
+    use_fancy_tab_bar = false,
+    window_decorations = "RESIZE",
+    unix_domains = {
+        {
+            name = "example",
+            proxy_command = { "ssh", "-T", "-A", "gpu1min", "wezterm", "cli", "proxy" },
+        },
+    },
+}
